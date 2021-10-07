2a9a3ea759a4d0a946093dce75b41c6ac7567df9
Rutuja Jakwad <rutujajakwad585@gmail.com>
2021-10-07 21:22:01 +0530

    Added : Code for equal,greater and less of two lines

--- Main.java
+++ Main.java
@@ -1,62 +1,30 @@
 package com.bl.linecomparison;
 
+import java.util.Scanner;
+
 public class Main {
-	public static void check(int x1, int x2, int y1, int y2,
-            int p1, int p2, int q1, int q2)
-{
-		int a1 = (y2 - y1) * p1 + (x1 - x2) * q1
-				+ (x2 * y1 - x1 * y2);
+	public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first  co-ordinate:");
+        Double firstLine = distanceCordinate(scanner.nextLine().split(""));
+        System.out.println("Enter second  co-ordinate:");
+        Double secondLine = distanceCordinate(scanner.nextLine().split(""));
 
-				if (a1 < 0) {
-			
-				int a2 = (y2 - y1) * p2 + (x1 - x2) * q2
-				    + (x2 * y1 - x1 * y2);
-
-				if (a2 >= 0)
-				System.out.println("Equal");
-
-				else if (a2 < 0)
-				System.out.println("Not Equal");
-				}
-
-				else if (a1 > 0) {
-
-				int a2 = (y2 - y1) * p2 + (x1 - x2) * q2
-				    + (x2 * y1 - x1 * y2);
-
-				if (a2 <= 0)
-				System.out.println("Intersecting");
-
-				else if (a2 > 0)
-				System.out.println("Not intersecting");
-				}
-
-			
-				else
-				System.out.println(
-				"points are lying on the line");
-				}
-				
-				public static void main(String args[])
-				{
-				
-				int x1 = 2, y1 = 3;
-				int x2 = 6, y2 = 4;
-
-				
-				System.out.println("Equation 1: (" + (y2 - y1)
-				          + ")x+(" + (x1 - x2) + ")y+("
-				          + (x2 * y1 - x1 * y2) + ") = 0");
-
-				int p1 = 3, q1 = 4;
-				int p2 = 7, q2 = 1;
-
-				
-				System.out.println("Equation 2: (" + (q2 - q1)
-				          + ")x+(" + (p1 - p2) + ")y+("
-				          + (p2 * q1 - p1 * q2) + ") = 0");
-
-				check(x1, x2, y1, y2, p1, p2, q1, q2);
-				}
+        if (firstLine.compareTo(secondLine)>0){
+            System.out.println("First Line Is Greater");
+        }
+        else if(firstLine.compareTo(secondLine)<0){
+            System.out.println("Second Line Is Greater");
+        }
+        else{
+            System.out.println("Both are Equal");
+        }
+    }
+    public static double distanceCordinate(String[] data){
+        double[] value=new double[4];
+        for(int i=0;i<data.length;i++)
+            value[i]=Double.parseDouble(data[i]);
+        return Math.sqrt(Math.pow((value[1]-value[0]),2)+ Math.pow((value[3]-value[2]),2));
+    }
 }
 
--- Point.java
+++ Point.java
@@ -1,9 +1,5 @@
 package com.bl.linecomparison;
 
 public class Point {
-	public int x1 , x2;
-	public int y1 , y2;
-	public int p1 , p2;
-	public int q1 , q2;
-
+	public int line;
 }
